76594a526d69ed8a286e3179c351a861a17ce9a5
Yao Zhang <58619665+zhangy-ict@users.noreply.github.com>
2022-11-07 16:15:19 +0800

    update parameters

--- mmformer/job.sh
+++ mmformer/job.sh
@@ -2,9 +2,9 @@
 pythonname='pytorch_1.2.0a0+8554416-py36tf'
 
 dataname='BRATS2018'
-pypath=./$pythonname
-cudapath=./cuda-9.0
-datapath=./${dataname}_Training_none_npy
+pypath=$pythonname
+cudapath=cuda-9.0
+datapath=${dataname}_Training_none_npy
 savepath=output
  
 export CUDA_VISIBLE_DEVICES=0,1,2,3
@@ -15,9 +15,8 @@ PYTHON=$pypath/bin/python3.6
 export PATH=$pypath/include:$pypath/bin:$PATH
 export LD_LIBRARY_PATH=$pypath/lib:$LD_LIBRARY_PATH
 
-$PYTHON train.py --batch_size=4 --datapath $datapath --savepath $savepath --num_epochs 1000 --lr 2e-4 --region_fusion_start_epoch 0 --dataname $dataname
+$PYTHON train.py --batch_size=4 --datapath $datapath --savepath $savepath --num_epochs 1000 --dataname $dataname
 
 #eval:
-# resume=output/model_last.pth
-# $PYTHON train.py --batch_size=2 --datapath $datapath --savepath $savepath --num_epochs 0 --lr 2e-4 --region_fusion_start_epoch 0 --dataname $dataname --resume $resume
-
+#resume=output/model_last.pth
+#$PYTHON train.py --batch_size=2 --iter_per_epoch 150 --datapath $datapath --savepath $savepath --num_epochs 300 --lr 2e-4 --region_fusion_start_epoch 20 --dataname $dataname --resume $resume
