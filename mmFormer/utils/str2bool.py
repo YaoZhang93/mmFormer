a555953be68c216ff86f8e19e7b83ba9f619891f
Yao Zhang <zhangyao215@mails.ucas.ac.cn>
2022-11-07 14:10:51 +0800

    Delete mmFormer directory

--- mmFormer/utils/str2bool.py
+++
@@ -1,8 +0,0 @@
-
-def str2bool(v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise ValueError('Unsupported value encountered.')
